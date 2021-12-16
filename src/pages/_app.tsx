1f4a427bb97196a9eabe7bef3b9ab901f00ee6f5
totto2727 <kaihatu.totto2727@gmail.com>
2021-12-16 15:36:40 +0000

    theme変更対応

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,31 +1,18 @@
 import './globals.css'
 
 import type { AppProps } from 'next/app'
-import Head from 'next/head'
-import { useState } from 'react'
+import { useEffect } from 'react'
+import { themeChange } from 'theme-change'
 
-type Modes = 'light' | 'dark'
 const MyApp = ({ Component, pageProps }: AppProps) => {
-  const [mode, setMode] = useState<Modes>('dark')
+  useEffect(() => {
+    themeChange(false)
+  }, [])
 
   return (
-    <>
-      <Head>
-        <html lang='ja' />
-      </Head>
-      <button
-        className='btn btn-primary'
-        onClick={() => {
-          setMode(mode === 'light' ? 'dark' : 'light')
-        }}>
-        Change Mode
-      </button>
-      <div data-theme={mode}>
-        <div className='m-0 h-0 font-sans box-border'>
-          <Component {...pageProps} />
-        </div>
-      </div>
-    </>
+    <div className='m-0 h-0 font-sans box-border'>
+      <Component {...pageProps} />
+    </div>
   )
 }
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,17 @@
 import type { NextPage } from 'next'
 
 const Home: NextPage = () => {
-  return <div className='text-3xl font-bold underline'>Hello world!</div>
+  return (
+    <>
+      <div className='text-3xl font-bold underline'>Hello world!</div>
+      <button
+        className='btn btn-primary'
+        data-toggle-theme='dark,light'
+        data-act-class='ACTIVECLASS'>
+        Change Mode
+      </button>
+    </>
+  )
 }
 
 export default Home
