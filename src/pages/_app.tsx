fd86c4e9ed76191a9e2cce6ea60c6594f3ea8ee2
totto2727 <kaihatu.totto2727@gmail.com>
2022-02-23 18:16:13 +0000

    AWS関係削除

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,24 +1,15 @@
 import './globals.css'
-import '@aws-amplify/ui-react/styles.css'
 
-import { Authenticator, useAuthenticator } from '@aws-amplify/ui-react'
-import Amplify from 'aws-amplify'
 import type { AppProps } from 'next/app'
 import type { ReactNode } from 'react'
 import { useEffect } from 'react'
 import { themeChange } from 'theme-change'
-
-import awsExports from '@/aws-exports'
-
-Amplify.configure(awsExports)
 
 type MainProps = { children: ReactNode }
 const Main = ({ children }: MainProps) => {
   useEffect(() => {
     themeChange(false)
   }, [])
-
-  const { signOut } = useAuthenticator()
 
   return (
     <div className='m-0 h-0 font-sans box-border'>
@@ -27,9 +18,6 @@ const Main = ({ children }: MainProps) => {
         <option value='light'>Light</option>
         <option value='dark'>Dark</option>
       </select>
-      <button className='btn btn-primary' onClick={signOut}>
-        SignOut
-      </button>
       {children}
     </div>
   )
@@ -41,15 +29,9 @@ const App = ({ Component, pageProps }: AppProps) => {
   }, [])
   return (
     <div>
-      <Authenticator>
-        {() => {
-          return (
-            <Main>
-              <Component {...pageProps} />
-            </Main>
-          )
-        }}
-      </Authenticator>
+      <Main>
+        <Component {...pageProps} />
+      </Main>
     </div>
   )
 }
