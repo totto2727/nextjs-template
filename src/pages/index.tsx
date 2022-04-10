05d5ab2f18616a98605dcdec9e58472153f23ac2
totto2727 <kaihatu.totto2727@gmail.com>
2022-04-10 08:13:43 +0000

    Fix:設定更新に伴うコードの変更

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,9 @@
 import type { NextPage } from 'next'
 
-const Home: NextPage = () => {
-  return (
-    <>
-      <div className='text-3xl font-bold underline'>Hello world!</div>
-    </>
-  )
-}
+const Home: NextPage = () => (
+  <>
+    <div className='text-3xl font-bold underline'>Hello world!</div>
+  </>
+)
 
 export default Home
