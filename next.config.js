b70c32348b9ae119d8d48c1d48168b8da7672e14
totto2727 <kaihatu.totto2727@gmail.com>
2023-03-03 23:56:41 +0900

    chore: ビルド可能な状態に修正

--- next.config.js
+++ next.config.js
@@ -1,19 +1,37 @@
-// @ts-check
-
 const {
   createVanillaExtractPlugin,
 } = require("@vanilla-extract/next-plugin");
 const withVanillaExtract = createVanillaExtractPlugin();
+const withMDX = require('@next/mdx')({
+  extension: /\.mdx?$/,
+  options: {
+    // If you use remark-gfm, you'll need to use next.config.mjs
+    // as the package is ESM only
+    // https://github.com/remarkjs/remark-gfm#install
+    remarkPlugins: [],
+    rehypePlugins: [],
+    // If you use `MDXProvider`, uncomment the following line.
+    // providerImportSource: "@mdx-js/react",
+  },
+})
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
   reactStrictMode: true,
   eslint: {
-    dirs: ["src"],
+    dirs: ["app", "src", ".storybook", "git"],
   },
   experimental: {
     appDir: true,
+    mdxRs: true,
+    typedRoutes: true,
+    turbo: {
+      loaders: {
+        '.svg': ['@svgr/webpack'],
+      },
+    },
   },
 };
 
-module.exports = withVanillaExtract(nextConfig);
+module.exports = withVanillaExtract(withMDX(nextConfig));
